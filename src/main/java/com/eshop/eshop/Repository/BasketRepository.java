a8504910e3d1877efc99a621ebd78995c65ae687
Dimitris Dimas <didimasmail@gmail.com>
2025-11-16 18:25:39 +0200

    Create Basket Repository

---
+++ src/main/java/com/eshop/eshop/Repository/BasketRepository.java
@@ -0,0 +1,11 @@
+package com.eshop.eshop.Repository;
+
+import com.eshop.eshop.entity.Basket;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+//I use repository bcs will extend crudRepository and will call crudRepository bcs
+//I don't have to make with sql repository
+@Repository
+public interface BasketRepository extends CrudRepository<Basket, String> {
+}
