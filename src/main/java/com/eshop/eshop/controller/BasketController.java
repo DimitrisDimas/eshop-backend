359a3f2c55622a2c678d6e09bfcf2a96557a8a2d
Dimitris Dimas <didimasmail@gmail.com>
2025-11-16 18:35:33 +0200

    Api for getting BasketById

--- src/main/java/com/eshop/eshop/controller/BasketController.java
+++ src/main/java/com/eshop/eshop/controller/BasketController.java
@@ -3,6 +3,7 @@ package com.eshop.eshop.controller;
 import com.eshop.eshop.dto.BasketResponse;
 import com.eshop.eshop.service.BasketService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,11 @@ public class BasketController {
     @GetMapping
     public List<BasketResponse> getAllBaskets() {
         return basketService.getAllBaskets();
+    }
+
+    @GetMapping("/{basketId}")
+    public BasketResponse getBasketById(@PathVariable String basketId){
+        return basketService.getBasketById(basketId);
     }
 
 }
