70ded658a8472afaa0252d1d0af776370b950105
Dimitris Dimas <didimasmail@gmail.com>
2025-11-16 18:37:27 +0200

    Api for creating a Basket

--- src/main/java/com/eshop/eshop/controller/BasketController.java
+++ src/main/java/com/eshop/eshop/controller/BasketController.java
@@ -1,10 +1,16 @@
 package com.eshop.eshop.controller;
 
+import com.eshop.eshop.dto.BasketItemResponse;
 import com.eshop.eshop.dto.BasketResponse;
+import com.eshop.eshop.entity.Basket;
+import com.eshop.eshop.entity.BasketItem;
 import com.eshop.eshop.service.BasketService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/baskets")
@@ -29,6 +35,43 @@ public class BasketController {
     @DeleteMapping("/{basketId}")
     public void deleteBasketById(@PathVariable String basketId){
         basketService.deleteBasketById(basketId);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<BasketResponse> createBasket(@RequestBody BasketResponse basketResponse){
+        //convert this basket response to basket entity
+        Basket basket = convertToBasketEntity(basketResponse);
+        //call the service method to create the basket
+        BasketResponse createdBasket = basketService.createBasket(basket);
+        //Return the Created Basket
+        return new ResponseEntity<>(createdBasket, HttpStatus.CREATED);
+    }
+
+    private Basket convertToBasketEntity(BasketResponse basketResponse) {
+        Basket basket = new Basket();
+        basket.setId(basketResponse.getId());
+        basket.setItems(mapBasketItemResponsesToEntities(basketResponse.getItems()));
+        return basket;
+    }
+
+    private List<BasketItem> mapBasketItemResponsesToEntities(List<BasketItemResponse> itemResponses) {
+        return itemResponses.stream()
+                .map(this::convertToBasketItemEntity)
+                .collect(Collectors.toList());
+    }
+
+    private BasketItem convertToBasketItemEntity(BasketItemResponse itemResponse) {
+        BasketItem basketItem = new BasketItem();
+        basketItem.setId(itemResponse.getId());
+        basketItem.setName(itemResponse.getName());
+        basketItem.setDescription(itemResponse.getDescription());
+        basketItem.setPrice(itemResponse.getPrice());
+        basketItem.setPictureUrl(itemResponse.getPictureUrl());
+        basketItem.setProductBrand(itemResponse.getProductBrand());
+        basketItem.setProductType(itemResponse.getProductType());
+        basketItem.setQuantity(itemResponse.getQuantity());
+        return basketItem;
     }
 
 }
