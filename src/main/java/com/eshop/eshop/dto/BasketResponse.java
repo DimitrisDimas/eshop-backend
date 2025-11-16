cd6f10d591b1a8a2aa8c0410cca996cf02492d97
Dimitris Dimas <didimasmail@gmail.com>
2025-11-16 18:21:14 +0200

    Add BasketResponse and BasketItemResponse DTOs

---
+++ src/main/java/com/eshop/eshop/dto/BasketResponse.java
@@ -0,0 +1,18 @@
+package com.eshop.eshop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BasketResponse {
+
+    private String id;
+    private List<BasketItemResponse> items;
+}
