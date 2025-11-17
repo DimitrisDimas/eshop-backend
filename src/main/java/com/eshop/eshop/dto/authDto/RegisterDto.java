a35a6311fccbf824c3883b4b161d16fc58c52b37
Dimitris Dimas <didimasmail@gmail.com>
2025-11-17 15:17:19 +0200

    Create Dto for Register and Login

---
+++ src/main/java/com/eshop/eshop/dto/authDto/RegisterDto.java
@@ -0,0 +1,39 @@
+package com.eshop.eshop.dto.authDto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class RegisterDto {
+
+    @NotEmpty
+    @Size(min = 5, message = "Name should have at least 5 characters")
+    @Schema(description = "Blog Register Name")
+
+    private String name;
+
+    @NotEmpty
+    @Size(min = 5, message = "Username should have at least 5 characters")
+    @Schema(description = "Blog Register Username")
+    private String username;
+
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email
+    @Schema(description = "Blog Register Email")
+    private String email;
+
+    @NotEmpty
+    @Size(min = 5, message = "Password should have at least 5 characters")
+    @Schema(description = "Blog Register Password")
+    private String password;
+}
