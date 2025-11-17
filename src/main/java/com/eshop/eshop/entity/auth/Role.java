6ed9cb6062783e7f81a403dd63bca8b137687636
Dimitris Dimas <didimasmail@gmail.com>
2025-11-17 15:13:47 +0200

    Create User and Role Entity

---
+++ src/main/java/com/eshop/eshop/entity/auth/Role.java
@@ -0,0 +1,27 @@
+package com.eshop.eshop.entity.auth;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="name",nullable = false)
+    private String name;
+
+
+
+}
